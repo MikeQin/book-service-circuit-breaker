857879c423427631c6666f41be13e493d0362945
Qin, Michael <Michael.Qin@tdameritrade.com>
2020-03-12 14:40:39 -0400

    init

---
+++ src/main/java/com/example/circuitbreaker/client/BookController.java
@@ -0,0 +1,17 @@
+package com.example.circuitbreaker.client;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BookController {
+	
+	@Autowired
+	private BookService bookService;
+
+	@RequestMapping("/to-read")
+	public String toRead() {
+		return bookService.readingList();
+	}
+}
